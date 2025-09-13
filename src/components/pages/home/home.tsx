d0a693131593af72e4bd670ba317df264ac9afc3
JLaxy <108315914+JLaxy@users.noreply.github.com>
2025-09-13 14:02:46 +0800

    checkpoint commit
    will transform into dockerized

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import Image from "next/image";
+import { FaFacebookSquare } from "react-icons/fa";
 
 export function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">HI! I&apos;M MAI</h1>
-      <Image
-        src={"/images/background.jpg"}
-        width={500}
-        height={500}
-        alt="mai photo"
-      />
+      {/* TITLE DIV*/}
+      <div
+        className="front-div text-right"
+        style={{ backgroundImage: `url('/images/mobile-background.jpg')` }}
+      >
+        <div className="mr-5">
+          <p className="text-[clamp(6rem,20vw,15rem)] title-text">MAI</p>
+          <p className="text-[clamp(3rem,10vw,7.5rem)] sub-text">VILLATURA</p>
+          <p className="text-[clamp(0.84375rem,2.8vw,2.125rem)] font-bold">
+            UGC PORTFOLIO
+          </p>
+        </div>
+        {/* SOCIALS DIV*/}
+        <div>
+          <div>
+            <FaFacebookSquare />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
