4c43d9645c40d635da108918c051ee0871f644c9
JLaxy <108315914+JLaxy@users.noreply.github.com>
2025-09-13 16:58:15 +0800

    initial hero
    initial globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Hero from "@/sections/hero";
+
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>Welcome to Next.js!</h1>
-    </>
+      {Hero()}
+    </div>
   );
 }
---
+++ src/sections/hero.tsx
@@ -0,0 +1,24 @@
+import { FaFacebookSquare } from "react-icons/fa";
+
+export default function Hero() {
+  return (
+    <div
+      className="front-div text-right"
+      style={{ backgroundImage: `url('/images/mobile-background.jpg')` }}
+    >
+      <div className="mr-5">
+        <p className="text-[clamp(6rem,20vw,15rem)] title-text">MAI</p>
+        <p className="text-[clamp(3rem,10vw,7.5rem)] sub-text">VILLATURA</p>
+        <p className="text-[clamp(0.84375rem,2.8vw,2.125rem)] font-bold">
+          UGC PORTFOLIO
+        </p>
+      </div>
+      {/* SOCIALS DIV*/}
+      <div>
+        <div>
+          <FaFacebookSquare />
+        </div>
+      </div>
+    </div>
+  );
+}
