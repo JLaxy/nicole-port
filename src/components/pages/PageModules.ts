517c0d7547fa5a73bce63299a94cfebe9d0750d5
JLaxy <108315914+JLaxy@users.noreply.github.com>
2025-09-13 16:15:41 +0800

    reinitilized nextjs

--- src/components/pages/PageModules.ts
+++
@@ -1,5 +0,0 @@
-import { About } from "./about/about";
-import { Contact } from "./contact/contact";
-import { Home } from "./home/home";
-
-export { Home, Contact, About };
--- src/components/pages/about/about.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-export function About() {
-  return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page.</p>
-    </div>
-  );
-}
