337a0a1405af4f70ecc48231297a60e787a5a850
JLaxy <108315914+JLaxy@users.noreply.github.com>
2025-09-26 19:45:03 +0800

    contact section

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import Socials from "@/components/socials";
 import "@/styles/contact.css";
 
 export default function Contact() {
-    return (
-        <div className="contact-section">
-            contact section
+  return (
+    <>
+      <article className="contact-section mb-8">
+        <section className="title-text">
+          LETS <span className="work-text">WORK</span> TOGETHER
+        </section>
+        <section className="sub-text">
+          {
+            "Ready to turn your brand's story into content people connect with? - I'd love to help you make that happen."
+          }
+        </section>
+        <div className="flex gap-5">
+          <button className="btn">{"Send me an Email"}</button>
+          <button className="btn">{"Let's Talk!"}</button>
         </div>
-    )
+      </article>
+      <Socials />
+    </>
+  );
 }
